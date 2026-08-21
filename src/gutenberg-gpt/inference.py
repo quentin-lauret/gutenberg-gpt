8f633a8e371adb1109678a5fccda33005322df35
quentin-lauret <quentinlauretp@gmail.com>
2026-08-21 18:46:45 +0400

    feat(inference): add a CLI

--- src/gutenberg-gpt/inference.py
+++ src/gutenberg-gpt/inference.py
@@ -1,15 +1,44 @@
-import torch
-from model import LanguageModel
-from gutenberg_tokenizer import GutenbergTokenizer
-from paths import MODEL_PATH, TOKENIZER_PATH, ARTIFACTS_DIRECTORY
+import argparse
 
-#torch.manual_seed(1337)
+import torch
+from gutenberg_tokenizer import GutenbergTokenizer
+from model import LanguageModel
+from paths import MODEL_PATH, TOKENIZER_PATH
+from prompt_toolkit.application import Application
+from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.layout import Layout, VSplit, Window
+from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
+from prompt_toolkit.layout.dimension import Dimension
+from prompt_toolkit.widgets import Frame
+import shutil 
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-o",
+    "--output",
+    required=False,
+    help="File to write the output to. If omitted, output is not saved.",
+    default=None
+)
+
+parser.add_argument(
+    "-nt",
+    "--nbtokens",
+    required=False,
+    help="Number of tokens to generate per round.",
+    default=100,
+    type=int
+)
+
+arguments = parser.parse_args()
+
+number_tokens = arguments.nbtokens
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 tokenizer = GutenbergTokenizer().load(str(TOKENIZER_PATH))
 vocabulary_size = tokenizer.VOCABULARY_SIZE
-
 
 checkpoint = torch.load(MODEL_PATH, map_location=device)
 
@@ -24,18 +53,63 @@ model = LanguageModel(
 
 model.load_state_dict(checkpoint["model_state_dict"])
 
-# optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
-# optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-
 model.eval()
 
 
-sentence = """A force d'aller en avant, il parvint au point où le brouillard de la fusillade devenait transparent. Si bien que les tirailleurs de la ligne rangés et à l'affût derrière leur levée de pavés, et les tirailleurs de la banlieue massés à l'angle de la rue, se montrèrent soudainement quelque chose qui remuait dans la fumée.Au moment où Gavroche débarrassait de ses cartouches un sergent gisant près d'une borne, une balle frappa le cadavre.- Fichtre ! dit Gavroche. Voilà qu'on me tue mes morts. Une deuxième balle fit étinceler le pavé à côté de lui. Une troisième renversa son panier. Gavroche regarda, et vit que cela venait de la banlieue."""
+def inference(text: str) -> str:
+    idx = torch.tensor([tokenizer.encode(text)], dtype=torch.long, device=device)
+    generated_text = tokenizer.decode(
+        model.generate(idx, number_tokens, EOT=tokenizer.EOT, temperature=1, top_k=10)[0].tolist()
+    )
+    return generated_text
 
-idx = torch.tensor([tokenizer.encode(sentence)], dtype=torch.long, device=device)
 
-print("Parameters :", sum(p.numel() for p in model.parameters()))
+def run_cli():
+    kb = KeyBindings()
 
-text = tokenizer.decode(model.generate(idx, 100, EOT=tokenizer.EOT, temperature=1, top_k=10)[0].tolist())
-with open(ARTIFACTS_DIRECTORY/"output.txt", "w") as file:
-    file.write(text)
+    @kb.add("enter")
+    def _(event):
+        event.app.exit(result=event.app.buffer.text)
+
+    @kb.add("c-c")
+    @kb.add("c-d")
+    def _(event):
+        event.app.exit(result=None)
+
+    def prompt():
+        buffer = Buffer(multiline=False)
+        zone = VSplit(
+            [
+                Window(FormattedTextControl("› "), width=2),
+                Window(
+                    BufferControl(buffer),
+                    height=Dimension(min=1, max=1),
+                    wrap_lines=True,
+                ),
+            ]
+        )
+        app = Application(
+            layout=Layout(Frame(zone)),
+            key_bindings=kb,
+            full_screen=False,
+            erase_when_done=True,
+        )
+        app.buffer = buffer
+        return app.run()
+
+    initial_text = ""
+    while True:
+        print()
+        prompted = prompt()
+        if not prompted:
+            break
+        text = inference(initial_text + prompted)
+        print(text[len(initial_text):], end="", flush=True)
+        initial_text = text
+        if arguments.output:
+            with open(arguments.output, "w") as file:
+                file.write(text)
+
+
+if __name__ == "__main__":
+    run_cli()
